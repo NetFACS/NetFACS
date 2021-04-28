f2ba6d01e008fb0ff580bc23a475bc9eb7a98a0e
AlexMielke1988 <52572112+AlexMielke1988@users.noreply.github.com>
2021-04-28 16:45:53 +0100

    added mutual information
    mutual information functions for elements and conditions

---
+++ R/mutual_information_condition.R
@@ -0,0 +1,74 @@
+#' Tests how much each element increases the specificity of all combinations it is in
+#'
+#' The function takes all elements and dyadic combinations of elements in a netfacs object, goes through all combinations these elements are in, and compares the specificity (strength with which the combination identifies the test condition) of all combinations with the element and the same combinations without the element, to test how much specificity the element adds when added to a signal. Only works for netfacs objects based on comparison between conditions.
+#'
+#'
+#' @param netfacs.data object resulting from netfacs() function
+#'
+#' @return Function returns a list with two data frames that include all elements and first-order combinations that occur at all, the number of combinations that each element/combination is part of, and how much adding this element to a combination adds on average to its specificity, and how often it occurs
+#'
+#' @importFrom Rfast rowmeans
+#' @export
+#'
+#' @examples
+#' ### how do angry facial expressions differ from non-angry ones?
+#' data(emotions_set)
+#' angry.face <- netfacs(
+#'  data = emotions_set[[1]],
+#'   condition = emotions_set[[2]]$emotion,
+#'   test.condition = "anger",
+#'   null.condition = NULL,
+#'   ran.trials = 100,
+#'   combination.size = 4
+#' )
+#'
+#' head(mutual.information.condition(angry.face), 20)
+
+
+mutual.information.condition <- function(netfacs.data){ 
+  
+  used.data <- netfacs.data$used.data$data
+  condition <- netfacs.data$used.data$condition
+  combinations <- netfacs.data$result$combination
+  combinations.unlist <- unlist(lapply(netfacs.data$result$combination, strsplit, split = '_'), recursive = F)
+  
+  condition.probs <- do.call(rbind, lapply(unique(condition), function(z){
+    
+    Aall <- mean(condition == z)
+    combination.probs <- do.call(rbind, lapply(1:length(combinations), function(x){
+      
+      if(length(combinations.unlist[[x]]) == 1){
+        Ball <- mean(used.data[,combinations.unlist[[x]]] == 1)
+        AandB <- sum(used.data[condition == z,combinations.unlist[[x]]] == 1)/nrow(used.data)
+        BgivenA <- mean(used.data[condition == z,combinations.unlist[[x]]] == 1)
+        count <- sum(used.data[condition == z,combinations.unlist[[x]]] == 1)
+      }
+      if(length(combinations.unlist[[x]]) > 1){
+        Ball <- mean(rowmeans(used.data[,combinations.unlist[[x]]]) == 1)
+        AandB <- sum(rowmeans(used.data[condition == z,combinations.unlist[[x]]]) == 1)/nrow(used.data)
+        BgivenA <- mean(rowmeans(used.data[condition == z,combinations.unlist[[x]]]) == 1)
+        count <- sum(rowmeans(used.data[condition == z,combinations.unlist[[x]]]) == 1)
+      }
+      
+      
+      mi <- round(log(
+        (AandB)/
+          (Aall * Ball))/
+          (-1 * log(AandB)), 3)
+      mi <- ifelse(is.na(mi), -1, mi)
+      
+      
+      return(data.frame(condition = z, 
+                        combination = combinations[x],
+                        combination.size = length(combinations.unlist[[x]]),
+                        count = count,
+                        conditional.probability = round(BgivenA, 3),
+                        mutual.information = mi))
+    }))
+    
+    return(combination.probs)
+  }))
+  
+  condition.probs <- condition.probs[order(condition.probs$mutual.information, decreasing = T),]
+  return(condition.probs) 
+}
---
+++ R/mutual_information_elements.R
@@ -0,0 +1,77 @@
+#' Calculates the pointwise mutual information of units with each other
+#'
+#' @param netfacs.data object resulting from netfacs() function
+#'
+#' @return Function returns a dataframe that includes all combinations, their occurrence counts and probabilities, and the pointwise mutual information (standardised between -1 and 1). 1 means seeing one necessitates seeing the other, -1 means one precludes the other
+#'
+#' @export
+#'
+#' @examples
+#' ### how do angry facial expressions differ from non-angry ones?
+#' data(emotions_set)
+#' angry.face <- netfacs(
+#'   data = emotions_set[[1]],
+#'   condition = NULL,
+#'   test.condition = NULL,
+#'   ran.trials = 100,
+#'   combination.size = 4
+#' )
+#'
+#' mutual.information(angry.face
+#' )
+
+mutual.information <- function(netfacs.data){
+  # select only combinations
+  net.data <- netfacs.data$result[netfacs.data$result$combination.size>1,]
+  
+  # go through each combination, calculate mutual informations for all iterations
+  results <- do.call(rbind, lapply(1:nrow(net.data), function(z){
+    #select elements
+    elements <- unlist(strsplit(net.data$combination[z], split = '_'))
+    
+    # make all possible combinations of one of the elements against all the others
+    element.lists <- lapply(1:length(elements), function(x){
+      A <- paste(elements[-x], collapse = '_')
+      B <- paste(elements[x], collapse = '_')
+      return(list(A = A, B = B))
+    })
+    # unlist it all
+    A <- as.vector(unlist(sapply(element.lists, function(x){x[1]})))
+    B <- as.vector(unlist(sapply(element.lists, function(x){x[2]})))
+    A <- ifelse(A=='', NA, A)
+    B[is.na(A)] <- NA
+    
+    # probability they occur together
+    AandB <- rep(net.data$observed.prob[z], length(A))
+    
+    # probability for first part only
+    Aall <- as.vector(sapply(A, function(x){
+      if(is.na(x)){return(NA)}
+      return(netfacs.data$result$observed.prob[netfacs.data$result$combination == x])
+    }))
+    
+    # probability of second part
+    Ball <- as.vector(sapply(B, function(x){
+      if(is.na(x)){return(NA)}
+      return(netfacs.data$result$observed.prob[netfacs.data$result$combination == x])
+    }))
+    
+    # calculate Mutual Information: log2(p(A&B)/(p(A) * p(B))) / (-1 * log2(p(A&B))
+    mi <- round(log2(
+      (AandB)/
+        (Aall * Ball))/
+      (-1 * log2(AandB)), 3)
+    
+    mi <- ifelse(is.na(mi), -1, mi)
+    
+    return(data.frame(combination1 = A, 
+                      element2 = B, 
+                      combination.size = net.data$combination.size[z], 
+                      count = net.data$count[z], 
+                      observed.prob = AandB, 
+                      mutual.information =  mi))
+  }))
+  
+  results <- results[order(results$mutual.information, decreasing = T),]
+  return(results)
+}
