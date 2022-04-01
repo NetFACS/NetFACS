9d2c191867fdf2f7934aa3baa8fff0f576523e7a
avrincon <37830256+avrincon@users.noreply.github.com>
2022-04-01 12:28:46 +0200

    re-code helpers

--- tests/testthat/test_probability_of_combination.R
+++ tests/testthat/test_probability_of_combination.R
@@ -1,7 +1,7 @@
 
 test_that("correct probabilities are calculated",{
   expect_snapshot_output(
-    calculate_prob_of_comb(
+    probability_of_combination(
       elements = list(c("c", "b", "i"), c("b", "e", "j", "g", "d", "h", "a"), c("i", "c", "b"), c("e", "j", "a", "d", "c", "i", "f", "b"), c("f", "h", "e", "j", "c", "g", "a"), c("j", "g", "b", "d", "a", "i", "c", "h"), c("b", "j", "c", "g", "f"), c("b", "d", "i", "g", "h", "a"), c("d", "e", "f"), c("j", "g", "e", "d", "b")), 
       maxlen = 2
     )
