fc32fab53744c44ec75fd3b585d9b12838adbd21
avrincon <37830256+avrincon@users.noreply.github.com>
2022-05-09 11:08:50 +0200

    add print method

---
+++ R/print.R
@@ -0,0 +1,22 @@
+#' Print method for objects of class \code{netfacs}
+#'
+#' @param x An object of class \code{netfacs}
+#'
+#' @export
+print.netfacs <- function(x) {
+  n_events <- nrow(x$used.data$data)
+  elements <- length(colnames(x$used.data$data))
+  n_combinations <- nrow(x$res)
+  max_comb_size <- max(x$result$combination.size)
+  method <- ifelse(is.null(x$used.data$condition), "Permutation", "Bootstrap")
+  n_ran_trials <- ncol(x$used.data$random.prob)
+  cat(
+    "A netfacs object: \n",
+    elements, "elements \n",
+    n_combinations, "element combinations \n",
+    "Maximum combination size:", max_comb_size, "\n",
+    n_events, "observations \n",
+    "Method:", method, "\n", 
+    n_ran_trials, "random trials"
+  )
+}
