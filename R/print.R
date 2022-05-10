66bdae8bf40b037a39475253d1d58b2508606ae9
avrincon <37830256+avrincon@users.noreply.github.com>
2022-05-10 19:10:41 +0200

    for compatibility with other methods

--- R/print.R
+++ R/print.R
@@ -1,9 +1,9 @@
 #' Print method for objects of class \code{netfacs}
 #'
 #' @param x An object of class \code{netfacs}
-#'
+#' @param ... Additional arguments that would be passed to or from other methods
 #' @export
-print.netfacs <- function(x) {
+print.netfacs <- function(x, ...) {
   n_events <- nrow(x$used.data$data)
   elements <- length(colnames(x$used.data$data))
   n_combinations <- nrow(x$res)
