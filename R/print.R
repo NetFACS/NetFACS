a0594215db1df709de93aa1f1a17f31427faa8a5
avrincon <37830256+avrincon@users.noreply.github.com>
2022-06-07 17:43:12 +0200

    print method for netfacs_multiple

--- R/print.R
+++ R/print.R
@@ -5,18 +5,35 @@
 #' @export
 print.netfacs <- function(x, ...) {
   n_events <- nrow(x$used.data$data)
-  elements <- length(colnames(x$used.data$data))
+  n_elements <- length(colnames(x$used.data$data))
   n_combinations <- nrow(x$res)
   max_comb_size <- max(x$result$combination.size)
-  method <- ifelse(is.null(x$used.data$condition), "Permutation", "Bootstrap")
-  n_ran_trials <- ncol(x$used.data$random.prob)
+  method <- attr(x, "stat_method")
+  n_ran_trials <- attr(x, "random_trials")
   cat(
     "A netfacs object: \n",
-    "Number of elements:", elements, "\n",
+    "Number of elements:", n_elements, "\n",
     "Number of element combinations:", n_combinations, "\n",
     "Maximum combination size:", max_comb_size, "\n",
     "Number of observations:", n_events, "\n",
-    "Method:", method, "\n", 
+    "Stat method:", method, "\n", 
+    "Random trials:", n_ran_trials
+  )
+}
+
+#' Print method for objects of class \code{netfacs_multiple}
+#'
+#' @param x An object of class \code{netfacs_multiple}
+#' @param ... Additional arguments that would be passed to or from other methods
+#' @export
+print.netfacs_multiple <- function(x, ...) {
+  n.conditions <- length(names(x))
+  method <- attr(x, "stat_method")
+  n_ran_trials <- attr(x, "random_trials")
+  cat(
+    "A netfacs_multiple object: \n",
+    "Number of conditions:", n.conditions, "\n",
+    "Stat method:", method, "\n", 
     "Random trials:", n_ran_trials
   )
 }
