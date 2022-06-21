c3a9f2606885418d424b8e269ffa337dae40982a
avrincon <37830256+avrincon@users.noreply.github.com>
2022-06-21 14:53:14 +0200

    add conditional_probabilities

--- R/conditional_probabilities.R
+++ R/conditional_probabilities.R
@@ -14,6 +14,7 @@
 #' @importFrom dplyr arrange
 #' @importFrom dplyr desc
 #' @importFrom dplyr filter
+#' @importFrom dplyr left_join
 #' @importFrom dplyr mutate
 #' @importFrom dplyr relocate
 #' @export
@@ -28,8 +29,8 @@
 #'   combination.size = 2
 #' )
 #' 
-#' summarise_pairs(angry.face)
-summarise_pairs <- function(netfacs.data) {
+#' conditional_probabilities(angry.face)
+conditional_probabilities <- function(netfacs.data) {
   
   if (class(netfacs.data) != "netfacs") {
     stop("Argument 'netfacs.data' must be of class 'netfacs'.")
@@ -41,57 +42,63 @@ summarise_pairs <- function(netfacs.data) {
     m <- netfacs.data$used.data$data
   }
   
+  # single elements
   elements <- colnames(m)
-  
-  # get dyadic combination of elements
-  d <- 
-    tidyr::expand_grid(elementA = elements,
-                       elementB = elements) %>% 
-    filter(.data$elementA != .data$elementB)
-  
-  
   # Total number of events will be different for each element if there are NAs
   N.events <- colSums(!is.na(m))
   N.obs <- colSums(m, na.rm = TRUE)
   p.elements <- N.obs / N.events
+
+
+  # get dyadic combination of elements
+  d <-
+    tidyr::expand_grid(elementA = elements,
+                       elementB = elements)%>%
+    dplyr::filter(.data$elementA != .data$elementB)
   
-  # count_AandB
-  N.pairs <-
-    apply(d, 1, function(x) {
-      sum(rowSums(m[, x], na.rm = TRUE) == 2)
-    }) 
-  
-  pA <- 
-    data.frame(elementA = elements,
-               p_A      = p.elements)
-  pB <- 
-    data.frame(elementB = elements,
-               p_B      = p.elements)
-  
-  d2 <- 
-    d %>% 
-    mutate(count = N.pairs) %>% 
-    left_join(pA, by = "elementA") %>% 
-    left_join(pB, by = "elementB")
-  
-  d3 <- 
-    d2 %>% 
-    mutate(
-      p_AandB   = .data$count / N.events,
-      p_AgivenB = .data$p_AandB / .data$p_B,
-      p_BgivenA = .data$p_AandB / .data$p_A,
-      pmi       = pmi(.data$p_AandB, .data$p_A, .data$p_B),
-      norm_pmi  = npmi(.data$pmi, .data$p_AandB)
+  pA <-
+    data.frame(
+      elementA = elements,
+      n_events_A = N.events,
+      p_A = p.elements
     )
+  pB <-
+    data.frame(
+      elementB = elements,
+      n_events_B = N.events,
+      p_B = p.elements
+    )
+  
+  d2 <-
+    d %>%
+    dplyr::left_join(pA, by = "elementA") %>%
+    dplyr::left_join(pB, by = "elementB")
+  
+  d3 <-
+    d2 %>%
+    dplyr::mutate(combination = paste(.data$elementA, .data$elementB, sep = "_"))
+  
+  # conditional probabilities/pmi must be calculated separately from complete cases of both elements
+  res <- 
+    lapply(1:nrow(d), function(x) {
+      conditional_probs(m, d$elementA[x], d$elementB[x])
+    }) %>% 
+    dplyr::bind_rows()
+  
+  d4 <- 
+    d3 %>% 
+    dplyr::left_join(res, by = c("elementA", "elementB"))
   
   # tidy
   out <- 
-    d3 %>% 
-    unite("combination", c("elementA", "elementB"), remove = FALSE) %>% 
-    relocate(.data$combination, .after = .data$elementB) %>% 
-    filter(.data$count > 0) %>% 
+    d4 %>% 
+    dplyr::filter(.data$n_AandB > 0) %>% 
     dplyr::mutate_if(is.double, round, 3) %>% 
-    arrange(desc(.data$count), .data$combination)
+    dplyr::arrange(dplyr::desc(.data$n_AandB), .data$combination) %>%
+    dplyr::select(
+      "elementA", "elementB", "combination", "n_AandB", "p_A", "p_B", "p_AandB",
+      "p_AgivenB","p_BgivenA", "pmi", "norm_pmi"
+    )
   
   return(out)
 }
@@ -110,3 +117,31 @@ npmi <- function(pmiAB, pAandB) {
   pmiAB / (-log2(pAandB))
 }
 
+conditional_probs <- function(m, A, B) {
+  mAB <- m[complete.cases(m[, c(A, B)]), c(A, B)]
+  
+  n_events <- nrow(mAB)
+  n_obs <- colSums(mAB)
+  p <- n_obs / n_events
+  pA <- as.numeric(p[A])
+  pB <- as.numeric(p[B])
+  
+  n_AandB <- sum(rowSums(mAB) == 2)
+  p_AandB <- n_AandB / n_events
+  
+  p_AgivenB <- p_AandB / pB
+  p_BgivenA <- p_AandB / pA
+  pmi_AandB <- pmi(p_AandB, pA, pB)
+  norm_pmi_AandB <- npmi(pmi_AandB, p_AandB)
+  
+  tibble::tibble(
+    elementA  = A,
+    elementB  = B,
+    n_AandB   = n_AandB,
+    p_AandB   = p_AandB,
+    p_AgivenB = p_AgivenB,
+    p_BgivenA = p_BgivenA,
+    pmi       = pmi_AandB,
+    norm_pmi  = norm_pmi_AandB
+  )
+} 
---
+++ tests/testthat/test_conditional_probabilities.R
@@ -0,0 +1,58 @@
+test_that("p A given B is correct", {
+  skip_on_cran()
+  
+  insert_na <- function(x, prob_na = 0.2) {
+    x[sample(c(TRUE, NA), 
+             prob = c(1-prob_na, prob_na), 
+             size = length(x), 
+             replace = TRUE)]
+  }
+  res <- 
+    netfacs_na(
+      data = d.sim.no.context,
+      condition = NULL,
+      ran.trials = 500,
+      combination.size = 2,
+      tail = "upper.tail",
+      use_parallel = TRUE, 
+      n_cores = detectCores()-1
+    )
+  res.na <- 
+    netfacs_na(
+      data = apply(d.sim.no.context, 2, insert_na),
+      condition = NULL,
+      ran.trials = 500,
+      combination.size = 2,
+      tail = "upper.tail",
+      use_parallel = TRUE, 
+      n_cores = detectCores()-1
+    )
+  
+  cp <- conditional_probabilities(res)
+  cp.na <- conditional_probabilities(res.na)
+  
+  p_8given3.cp <- cp %>% filter(combination == "8_3") %>% pull(p_AgivenB)
+  p_8given3.cp.na <- cp.na %>% filter(combination == "8_3") %>% pull(p_AgivenB)
+  
+  p_2given4.cp <- cp %>% filter(combination == "2_4") %>% pull(p_AgivenB)
+  p_2given4.cp.na <- cp.na %>% filter(combination == "2_4") %>% pull(p_AgivenB)
+  
+  norm_pmi_9and10.cp <- cp %>% filter(combination == "9_10") %>% pull(norm_pmi)
+  norm_pmi_9and10.cp.na <- cp.na %>% filter(combination == "9_10") %>% pull(norm_pmi)
+  
+  p_tol <- 0.2
+  # conditional probabilities
+  expect_equal(p_8given3.cp, 1)
+  expect_equal(p_8given3.cp.na, 1)
+  
+  expect_equal(p_2given4.cp, jp.no.context["4", "2"], tolerance = p_tol)
+  expect_equal(p_2given4.cp.na, jp.no.context["4", "2"], tolerance = p_tol)
+  
+  expect_equal(norm_pmi_9and10.cp, 1)
+  expect_equal(norm_pmi_9and10.cp.na, 1)
+  
+})
+
+
+
+
