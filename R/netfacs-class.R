2b1210a84820985ab7691e2c431e81ce7fd5cdae
avrincon <37830256+avrincon@users.noreply.github.com>
2022-07-07 16:21:49 +0200

    add get data helper

--- R/netfacs-class.R
+++ R/netfacs-class.R
@@ -1,3 +1,45 @@
+#' Print method for objects of class \code{netfacs}
+#'
+#' @param x An object of class \code{netfacs}
+#' @param ... Additional arguments that would be passed to or from other methods
+#' @export
+print.netfacs <- function(x, ...) {
+  n_events <- nrow(x$used.data$data)
+  n_elements <- length(colnames(x$used.data$data))
+  n_combinations <- nrow(x$res)
+  max_comb_size <- max(x$result$combination.size)
+  method <- attr(x, "stat_method")
+  n_ran_trials <- attr(x, "random_trials")
+  cat(
+    "A netfacs object: \n",
+    "Number of elements:", n_elements, "\n",
+    "Number of element combinations:", n_combinations, "\n",
+    "Maximum combination size:", max_comb_size, "\n",
+    "Number of observations:", n_events, "\n",
+    "Stat method:", method, "\n", 
+    "Random trials:", n_ran_trials,
+    "\n"
+  )
+}
+
+#' Print method for objects of class \code{netfacs_multiple}
+#'
+#' @param x An object of class \code{netfacs_multiple}
+#' @param ... Additional arguments that would be passed to or from other methods
+#' @export
+print.netfacs_multiple <- function(x, ...) {
+  n.conditions <- length(names(x))
+  method <- attr(x, "stat_method")
+  n_ran_trials <- attr(x, "random_trials")
+  cat(
+    "A netfacs_multiple object: \n",
+    "Number of conditions:", n.conditions, "\n",
+    "Stat method:", method, "\n", 
+    "Random trials:", n_ran_trials,
+    "\n"
+  )
+}
+
 #' Checks if argument is a \code{netfacs} object
 #'
 #' @param x An \R object
@@ -14,4 +56,37 @@ is.netfacs <- function(x) {
 #' @export
 is.netfacs_multiple <- function(x) {
   inherits(x, "netfacs_multiple")
+}
+
+#' Extract used data from a \code{netfacs} object
+#'
+#' @param x extract data from the test condition of a \code{netfacs} object
+#' @param condition one of "all" (default), "test" or "null".
+#' @export
+get_data <- function(x, 
+                     condition = "all") {
+  
+  m <- x$used.data$data
+  
+  if (condition == "all") {
+    return(m)
+  }
+  
+  if (condition == "test") {
+    if (attr(x, "stat_method") == "bootstrap") {
+      out <- m[x$used.data$condition == x$used.parameters$test.condition, ]
+      return(out)
+    } else {
+      return(m)
+    }
+  }
+  
+  if (condition == "null") {
+    if (attr(x, "stat_method") == "bootstrap") {
+      out <- m[x$used.data$condition == x$used.parameters$null.condition, ]
+      return(out)
+    } else {
+      return(NULL)
+    }
+  }
 }
